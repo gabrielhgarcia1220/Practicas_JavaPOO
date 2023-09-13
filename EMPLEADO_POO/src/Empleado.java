98aef3a591616c6da08e41eebf5f716402698f32
gabrielhgarcia1220 <gabrielhg1220@outlook.com>
2023-09-12 21:33:46 -0300

    Subiendo modificacion del proyecto Empleado

--- EMPLEADO_POO/src/Empleado.java
+++ EMPLEADO_POO/src/Empleado.java
@@ -1,9 +1,10 @@
 public class Empleado {
+
     private int id;
     private String nombre;
     private String apellido;
     private int salario;
-    private int incremento;
+    
 
     /* »»»» CONSTRUCTOR «««« */
     public Empleado(int id, String nombre, String apellido, int salario, int incremento) {
@@ -11,60 +12,41 @@ public class Empleado {
         this.nombre = nombre;
         this.apellido = apellido;
         this.salario = salario;
-        this.incremento = incremento;
     }
-    
-    /* »»»» GETTERS & SETTERS «««« */
+   
+    // GETTERS
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public String getApellido() {
         return apellido;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public String nombreCompleto(){
+        return (getNombre()+" "+getApellido());
     }
 
     public int getSalario() {
         return salario;
     }
+    
+    public int getSalarioAnual(){
+        return getSalario()*12;
+    }
 
+    // SETTER
     public void setSalario(int salario) {
         this.salario = salario;
     }
 
-    public int getIncremento() {
-        return incremento;
-    }
-
-    public void setIncremento(int incremento) {
-        this.incremento = incremento;
-    }
-
-    public int SalarioAnual(){
-        return (this.salario * 12);
-    }
-
-    public int aumentoSalario(){
-        return ((this.salario*incremento)/100)+this.salario;
-    }
-
-    public int salarioAnualTotal(){
-        return (aumentoSalario()*12);
+    // METODO
+    public int aumentoSalario(int incremento){
+        return getSalario()*incremento;
     }
 
     @Override
--- EMPLEADO_POO/src/Main.java
+++ EMPLEADO_POO/src/Main.java
@@ -3,45 +3,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         
-        int id = 0;
-        String nombre = "";
-        String apellido = "";
-        int salario = 0;
-        int incremento = 0;
+        int id;
+        String nombre;
+        String apellido;
+        int salario;
+        int incremento; // Porcentaje de incremento del sueldo
 
-        Scanner ingresoInt = new Scanner(System.in);
-        Scanner IngresoString = new Scanner(System.in);
-
-        Empleado nEmpleado = new Empleado(id, nombre, apellido, salario, incremento); // nEmpleado => nuevoEmpleado
-
-        
+        Scanner ingreso = new Scanner(System.in);
+                
         System.out.print("\nIngrese el ID del empleado: ");
-        nEmpleado.setId(ingresoInt.nextInt());
+        id = ingreso.nextInt();
+        ingreso.nextLine(); // Limpia la nueva línea después de leer un entero
 
         System.out.print("Ingrese el APELLIDO del empleado: ");
-        nEmpleado.setApellido(IngresoString.nextLine());
+        apellido = ingreso.nextLine();
 
         System.out.print("Ingrese el NOMBRE del empleado: ");
-        nEmpleado.setNombre(IngresoString.nextLine());
+        nombre = ingreso.nextLine();
 
         System.out.print("Ingrese el SALARIO del empleado:$ ");
-        nEmpleado.setSalario(ingresoInt.nextInt());
+        salario = ingreso.nextInt();
 
         System.out.print("Ingrese el PORCENTAJE de incremento salarial del empleado: ");
-        nEmpleado.setIncremento(ingresoInt.nextInt());
-        
+        incremento = ingreso.nextInt();
+
+        Empleado nEmpleado = new Empleado(id, nombre, apellido, salario, incremento); // nEmpleado => nuevoEmpleado
+        nEmpleado.aumentoSalario(incremento);
+
         System.out.println("|-----------------------------------------------------------------------|");
 
         System.out.println("\nApellido del empleado: " + nEmpleado.getApellido());
         System.out.println("Nombre del empleado : " + nEmpleado.getNombre());
         System.out.println("Salario:$ " + nEmpleado.getSalario());
-        System.out.println("El " + nEmpleado.getIncremento() + "% del salario mensual:$ " + (nEmpleado.getSalario()*nEmpleado.getIncremento())/100);
-        System.out.println("Incremento del " + nEmpleado.getIncremento() + "% del salario mensual:$ " + nEmpleado.aumentoSalario() + "\n");
-
+        System.out.println("El " + incremento + "% del salario mensual:$ " + (nEmpleado.getSalario()*incremento)/100 + "\n");
+    
         System.out.println("|-----------------------------------------------------------------------|");
 
-        System.out.println("\n>>> Salario Anual:$ " + nEmpleado.SalarioAnual());
-        System.out.println(">>> Salario anual con el incremento del " + nEmpleado.getIncremento() + "% : $" + nEmpleado.salarioAnualTotal() + "\n");
-
+        System.out.println("\n>>> Salario Anual:$ " + nEmpleado.getSalarioAnual() + "\n");
+        
     }
 }
